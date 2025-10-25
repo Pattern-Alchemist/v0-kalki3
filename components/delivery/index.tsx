f96f70bc57fafa4c1cb50c856ee225439fc845cf
Pattern-Alchemist <pattern.alchemist.7@gmail.com>
2025-10-25 17:45:42 +0530

    Create index.tsx for delivery components

---
+++ components/delivery/index.tsx
@@ -0,0 +1,6 @@
+export { PDFGenerator } from './pdf-generator'
+export { WhatsAppShare } from './whatsapp-share'
+export { EmailShare } from './email-share'
+export { DownloadButton } from './download-button'
+export { PrintButton } from './print-button'
+export { ShareButton } from './share-button'
